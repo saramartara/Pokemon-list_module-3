8242c0b593e5d4f29f0b21c10817016c2e7237b4
saramartara <saramartinezaragon@gmail.com>
2021-02-24 10:22:04 +0100

    keep data in state at App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import data from '../data/data.json';
 import PokeList from './PokeList';
 
 const App = () => {
-  // const [pokemon, setPokemon] = useState(data);
-  // console.log(useState);
+  const [pokemon] = useState(data);
+  console.log(useState);
 
   return (
     <div className="App">
       <h1>Mi lista de Pokemon</h1>
-      <PokeList getPokemons={data} />
+      <PokeList getPokemons={pokemon} />
     </div>
   );
 };
